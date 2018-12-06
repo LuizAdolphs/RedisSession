323c47c43300b2a5e1174af42d6b6fa0ecdfa16c
Luiz Adolphs <luiz.adolphs@gmail.com>
2018-12-05 22:51:03 -0200

    Initial Commit

---
+++ app/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using app.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            var instance = Environment.GetEnvironmentVariable("ASPNETCORE_INSTANCE_NAME");
+
+            HttpContext.Session.SetString("KeyOfSessionValue", $"Some nice value added in the instance {instance}, maybe an object serialized to JSON?");
+
+            await HttpContext.Session.CommitAsync();
+
+            ViewData["Message"] = "If you see this, I guess the data was stored ";
+
+            return View();
+        }
+
+        public async Task<IActionResult> Retrieve()
+        {
+
+            await HttpContext.Session.LoadAsync();
+
+            string storedValue = HttpContext.Session.GetString("KeyOfSessionValue");
+            
+            if (String.IsNullOrEmpty(storedValue))
+            {
+                storedValue = "What a shame, no session value for you.";
+            }
+
+            var instance = Environment.GetEnvironmentVariable("ASPNETCORE_INSTANCE_NAME");
+
+            ViewData["Message"] = $"{storedValue} And you are in instance: {instance}";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ app/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace app
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var redis = ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable("REDIS"));
+            
+            services
+                .AddDataProtection()
+                .PersistKeysToStackExchangeRedis(redis, "DataProtectionKeys");
+
+            services.AddStackExchangeRedisCache(option =>
+            {
+                option.Configuration = Environment.GetEnvironmentVariable("REDIS");
+                option.InstanceName = "RedisInstance";
+            });            
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.Name = "AppTest";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            
+            app.UseSession();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
